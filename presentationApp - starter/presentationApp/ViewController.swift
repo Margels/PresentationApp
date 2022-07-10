9774e272091dd427ae08939511830cf74ced2dbe
Margels <89708428+Margels@users.noreply.github.com>
2022-07-10 17:35:49 +0200

    Add files via upload

---
+++ presentationApp - starter/presentationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  presentationApp
+//
+//  Created by Martina on 05/07/22.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+}
